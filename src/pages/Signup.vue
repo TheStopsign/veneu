e1c05ba7c114871f77361d069e904bec86652726
TheStopsign <ejwhitton43@gmail.com>
2021-09-12 20:05:51 -0400

    ios keyboard fix attempt

--- src/pages/Signup.vue
+++ src/pages/Signup.vue
@@ -19,6 +19,7 @@
               class="text-primary q-pa-none q-ma-none q-mt-md"
               v-model="email"
               label="Email"
+              @focus="onFocus"
             >
               <template v-slot:prepend>
                 <q-icon name="email" />
@@ -48,6 +49,10 @@ export default {
     };
   },
   methods: {
+    onFocus() {
+      window.scrollBy(0, 0);
+      document.body.scrollTop = 0;
+    },
     formValid() {
       return this.isValidEmail(this.email);
     },
