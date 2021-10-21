10bbe425a60feca70246a2b3ba4e7c01268f6242
TheStopsign <ejwhitton43@gmail.com>
2021-10-21 19:07:28 -0400

    maybe larger mobile cam scan

--- src/pages/CheckinScan.vue
+++ src/pages/CheckinScan.vue
@@ -58,17 +58,7 @@
           {{ !last ? "Searching for QR Code..." : "QR Code found! Keep scanning..." }}
         </div>
       </div>
-      <div v-if="$q.screen.lt.sm">
-        <video id="captured-screen" autoplay :style="screen_scanning ? '' : 'display: none'"></video>
-        <video
-          v-if="camera_scanning"
-          id="camera-video"
-          autoplay
-          style="display: flex; flex-direction: row"
-          class="q-pa-md neu-convex"
-        ></video>
-      </div>
-      <q-responsive v-else :ratio="16 / 9" style="max-height: 50vh">
+      <q-responsive :ratio="$q.screen.lt.sm ? undefined : 16 / 9" style="max-height: 50vh; max-width: 100%">
         <video id="captured-screen" autoplay :style="screen_scanning ? '' : 'display: none'"></video>
         <video
           v-if="camera_scanning"
@@ -78,6 +68,16 @@
           class="q-pa-md neu-convex"
         ></video>
       </q-responsive>
+      <!-- <q-responsive v-else :ratio="16 / 9" style="max-height: 50vh">
+        <video id="captured-screen" autoplay :style="screen_scanning ? '' : 'display: none'"></video>
+        <video
+          v-if="camera_scanning"
+          id="camera-video"
+          autoplay
+          style="display: flex; flex-direction: row"
+          class="q-pa-md neu-convex"
+        ></video>
+      </q-responsive> -->
     </div>
     <ApolloSubscribeToMore
       v-if="user"
