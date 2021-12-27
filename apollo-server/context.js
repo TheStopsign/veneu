6916e84598cee2f573bd9e3ee5a19d0b6d97fa0a
TheStopsign <ejwhitton43@gmail.com>
2021-12-26 22:25:05 -0500

    cache

--- apollo-server/context.js
+++ apollo-server/context.js
@@ -2,15 +2,24 @@ const getModels = require("./models");
 const DataLoader = require("dataloader");
 const jwt = require("jsonwebtoken");
 
-const getUser = async (models, token) =>
-  jwt.verify(token, process.env.JWTAUTH_KEY, function (err, decoded) {
-    return err || !decoded
-      ? null
-      : models.User.findOne({ _id: decoded._id }).then((user) => {
-          return models.Auth.find({ _id: { $in: user._doc.auths } }).then((auths) => {
-            return { ...user._doc, auths };
-          });
-        });
+const getUser = async (models, { Auth: AuthsCache, User: UsersCache }, token) =>
+  jwt.verify(token, process.env.JWTAUTH_KEY, async function (err, decoded) {
+    if (err || !decoded) {
+      return null;
+    }
+    let user = UsersCache.get(decoded._id + "");
+    if (!user) {
+      user = await models.User.findOne({ _id: decoded._id });
+      UsersCache.set(decoded._id + "", user);
+    }
+    let auths = user._doc.auths.map((a) => AuthsCache.get(a + ""));
+    if (auths.filter((a) => a == null).length) {
+      auths = await models.Auth.find({ _id: { $in: user._doc.auths } });
+      await auths.forEach(function (auth) {
+        AuthsCache.set(auth._id + "", auth);
+      });
+    }
+    return { ...user._doc, auths };
   });
 const findForModel = (models, keys, modelName) => models[modelName].find({ _id: { $in: keys } });
 const cacheKeyFn = (key) => key.toString();
@@ -37,7 +46,7 @@ module.exports = (pubsub, modelNames, models, caches) => {
     } else {
       const auth = (req.headers && req.headers.authorization && req.headers.authorization.split(" ")) || [];
       if (auth.length == 2 && auth[0] == "Bearer") {
-        const user = await getUser(models, auth[1]);
+        const user = await getUser(models, caches, auth[1]);
         const auths = user.auths.filter((a) => undefined != a.role && null != a.role);
         return {
           requester: {
